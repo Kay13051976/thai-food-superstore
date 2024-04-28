05a168eb03fef1ca46f5ae07fb1facdf869a14e3
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-04-28 16:52:52 +0000

    Created comment section and functional it in homepage

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class RecipeCommments(models.Model):
+
+    recipeid = models.CharField(max_length=50)
+    username = models.CharField(max_length=150)
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.recipeid
--- home/views.py
+++ home/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from . models import RecipeCommments
 # Create your views here.
+
 
 def index(request):
     """ A view to return the index page """
-    
-    return render(request, 'home/index.html')
+# comment
+    RecipeComm = RecipeCommments.objects.all().order_by('-id')
+    if request.user.is_authenticated:
+
+        if request.method == 'GET':
+            allcoments = RecipeCommments.objects.all().count()
+            newid = allcoments+1
+            if 'comment' in request.GET:
+                # form = SubscibersForm(request.POST)
+                RecipeCommments_new = RecipeCommments(
+                    newid, request.GET['recipeid'], str(request.user), request.GET['comment'])
+                RecipeCommments_new.save()
+            # messages.success(request, 'Subscription Successful')
+                return redirect('home')
+
+    context = {
+        'RecipeCommments': RecipeComm,
+    }
+
+    return render(request, 'home/index.html', context)
