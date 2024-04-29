b0db23ae580e43cd9de790e795840a6c46c8522f
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-04-29 17:39:43 +0000

    Added if else statement to mail nav link letter.views.py

--- letter/views.py
+++ letter/views.py
@@ -1,5 +1,6 @@
+
 from django.shortcuts import render, redirect , reverse, get_object_or_404
-from . models import Subscribers, MailMessage
+from . models import Subscribers,MailMessage
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.db.models import Q
@@ -30,40 +31,45 @@ def mail_letter(request):
     #queries = Q(email=query)
     #df = emails
     #read_frame(emails, fieldnames=['email'])
-    emails = Subscribers.objects.all()
-    df = pd.DataFrame(
-        list(
-            Subscribers.objects.all().values(
-                "email"
-            )
-        )
-    )
-    #read_frame(emails, fieldnames=['email'])
-    mail_list = df['email'].values.tolist()
-
+    emails = Subscribers.objects.all().count()
     #mail_list = Subscribers.email.values.tolist()
-    print(mail_list)
-    if request.method == 'GET':
-        allMailMessage = MailMessage.objects.all().count()
-        newid = allMailMessage + 1
+    #print(mail_list)
+    if request.user.is_superuser:
+        if request.method == 'GET':
+            if emails>0:
+                df = pd.DataFrame(
+                list(
+                    Subscribers.objects.all().values(
+                        "email"
+                        )
+                    )
+                )
+        # read_frame(emails, fieldnames=['email'])
+                mail_list = df['email'].values.tolist()
+
+                allMailMessage = MailMessage.objects.all().count()
+                newid = allMailMessage + 1
         #form = MailMessageForm(request.POST)
-        if 'titleletter' in request.GET:
+                if 'titleletter' in request.GET:
 
-            title = request.GET['titleletter']
-            message = request.GET['messageletter']
+                    title = request.GET['titleletter']
+                    message = request.GET['messageletter']
 
-            message_new = MailMessage(newid,title,message)
-            message_new.save()
-            send_mail(
-                title,
-                message,
-                'testsmtpmail@sns-autotransport.com',
-                mail_list,
-                fail_silently=False,
-            )
-            messages.success(request, 'Message has been sent to the Mail List')
-            return redirect('mail-letter')
-
+                    message_new = MailMessage(newid,title,message)
+                    message_new.save()
+                    send_mail(
+                    title,
+                    message,
+                    'testsmtpmail@sns-autotransport.com',
+                    mail_list,
+                    fail_silently=False,
+                    )
+                    messages.success(request, 'Message has been sent to the Mail List')
+                    return redirect('mail-letter')
+            else:
+                messages.success(request, 'No Emails Found of newsletter where you send newsletter, First need to subscribe for newsletter')
+    else:
+        messages.success(request, 'This feature can use only superuser. Thank!')
     context = {
         'MailMessage': MailMessage,
     }
