5c511712c6c9eefcf8d615de2171c97e76619bc4
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-05-01 15:23:42 +0000

    Added urlpattert attribute to thai_food_superstore.settings.py

--- thai_food_superstore/urls.py
+++ thai_food_superstore/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('letter-index/', include('letter.urls')),
     path('checkout/', include('checkout.urls')),
+     path('recepies/', include('recepies.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
